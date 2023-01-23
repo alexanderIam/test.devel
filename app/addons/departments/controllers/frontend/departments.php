fbea6a49e901c7ed9ce36473febfa8b2e37dc414
Ditto <nevergiveup9051@gmail.com>
2023-01-23 22:15:55 +0500

    SCHOOL-385 : Departments : Modified frontend controller

--- app/addons/departments/controllers/frontend/departments.php
+++ app/addons/departments/controllers/frontend/departments.php
@@ -12,67 +12,34 @@
  * "copyright.txt" FILE PROVIDED WITH THIS DISTRIBUTION PACKAGE.            *
  ****************************************************************************/
 
+ use Tygh\Registry;
+ 
 if (!defined('BOOTSTRAP')) { die('Access denied'); }
 
 if ($mode === 'departments') {
-
     Tygh::$app['session']['continue_url'] = 'departments.departments';
 
     $params = $_REQUEST;
-    $params['user_id'] = Tygh::$app['session']['auth']['user_id'];
+    $params['usergroup_ids'] = Tygh::$app['session']['auth']['usergroup_ids'];
 
-    if (empty($params['user_id'])) {
-        return [CONTROLLER_STATUS_DENIED];
-    }
+    list($departments, $search) = fn_get_departments($params, Registry::get('addons.departments.departments_per_page'), CART_LANGUAGE);
 
-    if ($sort_by = fn_change_session_param(Tygh::$app['session'], $_REQUEST, 'sort_by')) {
-        $params['sort_by'] = $sort_by;
-    }
-
-    if ($sort_order = fn_change_session_param(Tygh::$app['session'], $_REQUEST, 'sort_order')) {
-        $params['sort_order'] = $sort_order;
-    }
-
-    list($departments, $search) = fn_get_departments($params, DEPARTMENTS_PER_PAGE, CART_LANGUAGE);
-    
     Tygh::$app['view']->assign([
         'departments' => $departments,
         'search' => $search,
         'columns' => 3,
     ]);
 
-    fn_add_breadcrumb(__('departments_title'), Tygh::$app['session']['continue_url']);
+    fn_add_breadcrumb(__('departments_title'));
 
 } elseif ($mode === 'department') {
-
     Tygh::$app['session']['continue_url'] = 'departments.departments';
 
     $department_data = [];
     $department_id = !empty($_REQUEST['department_id']) ? $_REQUEST['department_id'] : 0;
-    $department_data['user_id'] = Tygh::$app['session']['auth']['user_id'];
-
-    if (empty($department_data['user_id'])) {
-        return [CONTROLLER_STATUS_NO_PAGE];
-    }
-
     $department_data = fn_get_department_data($department_id, DESCR_SL);
-
+    
     fn_add_breadcrumb(__('departments_title'), Tygh::$app['session']['continue_url']);
-    $params = $_REQUEST;
-    $params['extend'] = ['description'];
-    $params['user_ids'] = !empty($department_data['user_ids']) ? implode(',', $department_data['user_ids']) : -1;
-
-    if ($items_per_page = fn_change_session_param(Tygh::$app['session']['search_params'], $_REQUEST, 'items_per_page')) {
-        $params['items_per_page'] = $items_per_page;
-    }
-
-    if ($sort_by = fn_change_session_param(Tygh::$app['session']['search_params'], $_REQUEST, 'sort_by')) {
-        $params['sort_by'] = $sort_by;
-    }
-
-    if ($sort_order = fn_change_session_param(Tygh::$app['session']['search_params'], $_REQUEST, 'sort_order')) {
-        $params['sort_order'] = $sort_order;
-    }
-
+ 
     Tygh::$app['view']->assign('department_data', $department_data);
 }
